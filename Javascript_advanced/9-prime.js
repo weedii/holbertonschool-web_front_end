de75cfe4114ae22770f342eb8dda0930d98dabf7
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-09-12 11:38:59 +0100

    Prime numbers & timing execution

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,27 @@
+const { performance } = require("perf_hooks");
+
+let count = 0;
+function countPrimeNumbers() {
+  for (let i = 2; i <= 100; i++) {
+    let check = 1;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        check = 0;
+        break;
+      }
+    }
+    if (check == 1) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+let start = performance.now();
+countPrimeNumbers();
+let timeTaken = performance.now() - start;
+console.log(
+  "Execution time of printing countPrimeNumbers was " +
+    timeTaken +
+    " milliseconds."
+);
